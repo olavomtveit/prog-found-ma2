379914153b7974fd80aec42ed294e440a30b25ed
olavomtveit <56303134+olavomtveit@users.noreply.github.com>
2020-01-19 16:27:24 +0100

    ma2 questions
    Module Assignmnet Question 1-10

---
+++ js/script.js
@@ -0,0 +1,43 @@
+//Question 1
+//var petArray = ["cat", "cow", "dog"];
+
+//Question 2
+//console.log(petArray[1]);
+
+//Question 3
+//console.log(petArray.push("sheep"));
+
+//Question 4
+//console.log(petArray.length);
+
+//Question 5
+//var catObject = {
+//    name: "Not a cat",
+//    color: "Brown",
+//    age: "4",
+//};
+
+//Question 6
+//console.log(petObject["age"]);
+
+//Question 7
+//var catArray = [{
+//    name: "Not a cat",
+//    color: "brown",
+//    age: "4",
+//}]
+
+//Question 8
+//var catArray = ["Not a cat", "brown", "4"];
+//for (var i=0; i<catArray.length; i++){
+//    console.log(catArray[i]);
+//}
+
+//Question 9
+//function logToConsole(functionTest){
+//    console.log(functionTest);
+//}
+//logToConsole("Question 9");
+
+//Question 10
+//logToConsole("Donkey");
